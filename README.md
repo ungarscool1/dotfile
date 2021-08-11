e88f56d6d2f7574516754686071036aa7cf17f77
Léo Godard <leo@elcomputor.local>
2021-08-12 01:47:52 +0200

    Rework on dotfiles

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-# Mon dotfile
-Tu dotfile es mi dotfile.
+# Léo's dotfile
 
-## Quel OS as-tu ?
-J'ai Antergos 18.10
-![Mon screenfetch](https://i.imgur.com/ZaXzyKe.png)
-## A qui s'adresse se repo ?
-Se repo s'adresse à son créateur mais vous pouvait vous en servir affin d'avoir ma config Arch
-## Peux-tu me passé une clé de license IDEA ?
-Non, de toute façon j'utilise la version community
-## Comment se servir de ungs-utils ?
-Tout est écrit sur [ungarscool1/ungs-bash](https://github.com/ungarscool1/ungs-bash)
-## Comment installer ton dotfile ?
-Pas besoin de mettre la commande sudo avant ``sh install.sh`` car sudo est inclu à chaque commande dans le fichier d'installation.
-`` wget https://raw.githubusercontent.com/ungarscool1/dotfile/master/install.sh && sh install.sh``
-## J'ai un bug !
-Si vous avez un bug il y a la section issue pour me prévenir du bug
+![neofetch](https://raw.githubusercontent.com/ungarscool1/dotfiles/master/image/neofetch.png)
+
+## Installation
+**Warning** If you want use this dotfile repository, you need to review the code, configuration file.
+Don't use it blindly. Use at your own risk.
+
+### Install script
+
+`` sh -c "$(curl -fsSL https://raw.github.com/ungarscool1/dotfile/master/install.sh)"``
+
+## Feedbacks are welcome
+Any suggestions, improvements, start an [issue](https://github.com/ungarscool1/dotfile/issues)/[pull request](https://github.com/ungarscool1/dotfile/pulls).
--- install.sh
+++ install.sh
@@ -1,193 +1,58 @@
-DIALOG=${DIALOG=dialog}
+echo -ne "Are-you sure you want to install the script? [y/n] "
+read -n 1 -s REPLY
+if [ "$REPLY" = "y" ]; then
+	echo -ne "\nInstalling...\n"
+else
+	echo -ne "\nAborting\n"
+	exit 0
+fi
 
-
-$DIALOG --clear --title " Avertissement " --clear --yesno "Le dotfile est customiser pour mon utilisation !\nL'installateur est a utiliser à vos riques et périls !\nÊtes-vous d'accord pour installer le dotfile ?" 25 50
-
-case $? in
-	0) echo "Requesting sudoeur...";;
-	1)	exit;;
-	255)	exit;;
-esac
-
-sudo echo "Votre mot de passe est enregistrer pour la durée de l'installation"
+echo "Requesting sudo password"
+sudo echo "Your sudo password is registered while installing the script"
+if [ "$?" -ne 1 ]; then
+	echo "You refused to type your sudo password"
+	exit 1
+fi
 
 # Installation
 
-echo "Installation de yaourt, wget et dialog..."
-sudo pacman -Sq yaourt wget dialog
-echo "Terminé"
-echo " "
-echo " "
-compteur=0
-(
-while test $compteur != 150
-do
-echo $compteur
+echo "Installing wget zsh git"
+sudo pacman -Sq wget git zsh
 
-if (( $compteur == 0 )) ; then
-	echo "XXX"
-	echo "Téléchargement de packages.list..."
-	echo "XXX"
-	wget -q https://github.com/ungarscool1/dotfile/raw/master/packages/packages.list
-elif (( $compteur == 50 )) ; then
-	echo "XXX"
-	echo "Téléchargement de games.list..."
-	echo "XXX"
-	wget -q https://github.com/ungarscool1/dotfile/raw/master/packages/games.list
-else
-	echo "Terminé"
-fi
+echo "Installing plasma"
+sudo pacman -S xorg sddm
+sudo pacman -S plasma kde-applications
+sudo systemctl enable sddm
+sudo systemctl enable NetworkManager
+sudo wget -q -O /usr/lib/sddm/sddm.conf.d/default.conf https://raw.githubusercontent.com/ungarscool1/dotfile/master/conf/sddm.conf
 
+echo "Installing dev tools"
+sudo pacman -S base-devel
+git clone https://github.com/longld/peda.git ~/peda
+echo "source ~/peda/peda.py" >> ~/.gdbinit
 
-compteur=`expr $compteur + 50`
-done
-) | $DIALOG --gauge "Récuperation des fichier à télécharger..." 20 70 0
-
-compteur=0
-(
-while test $compteur != 150
-do
-echo $compteur
-
-if (( $compteur == 0 )) ; then
-	echo "XXX"
-	echo "Téléchargement des clés de confiances pour la compilation de discord..."
-	echo "XXX"
-	gpg --recv-keys --keyserver hkp://pgp.mit.edu 0FC3042E345AD05D
-elif (( $compteur == 50 )) ; then
-	echo "XXX"
-	echo "Ajout des clés de confiances pour la compilation de discord"
-	echo "XXX"
-else
-	echo "Terminé"
-fi
-
-
-compteur=`expr $compteur + 50`
-done
-) | $DIALOG --title "Téléchargement" --gauge "Récuperation des fichier à télécharger..." 20 70 0
-
-
-##
-## Installation of packages.list
-##
-
-packagesToInstall=$(cat packages.list | wc -l)
-compteur=1
-(
-while test $compteur != $(($packagesToInstall+1))
-do
-percent=$(awk -vn=$compteur -vx=$packagesToInstall 'BEGIN{print(n/x*100)}')
-package=$(sed -n "${compteur}p" < packages.list)
-echo $percent
-if (( $compteur == $(($packagesToInstall+1)))); then
-    echo "XXX"
-    echo "Terminé"
-    echo "XXX"
-else
-    echo "XXX"
-    echo "Installation de $package"
-    echo "XXX"
-    sudo yaourt -Ss $package --needed --noconfirm --quiet
-fi
-
-compteur=`expr $compteur + 1`
-sleep 1
-done
-) | dialog --gauge "Récuperation des fichier à télécharger..." 6 60 0
-
-##
-## Installation of games.list
-##
-
-packagesToInstall=$(cat games.list | wc -l)
-compteur=1
-(
-while test $compteur != $(($packagesToInstall+1))
-do
-percent=$(awk -vn=$compteur -vx=$packagesToInstall 'BEGIN{print(n/x*100)}')
-package=$(sed -n "${compteur}p" < games.list)
-echo $percent
-if (( $compteur == $(($packagesToInstall+1)))); then
-    echo "XXX"
-    echo "Terminé"
-    echo "XXX"
-else
-    echo "XXX"
-    echo "Installation de $package"
-    echo "XXX"
-    sudo yaourt -Ss $package --needed --noconfirm --quiet
-fi
-
-compteur=`expr $compteur + 1`
-sleep 1
-done
-) | dialog --gauge "Récuperation des fichier à télécharger..." 6 60 0
+wget -q https://github.com/ungarscool1/dotfile/raw/master/packages/packages.list
+echo "Installing packages"
+yay -S --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --noremovemake --cleanafter $(cat packages.list)
 
 
 # Configuration
 
-$DIALOG --title " Configuration " --clear --yesno "Voulez-vous configurer Gnome ?" 25 50
+echo "-- Configuring the script --"
+echo -ne " |- discord (please close discord after login)"
+discord
+echo -ne "\r |- discord\n"
+echo -ne " |- spotify (please close spotify after login)"
+spotify
+echo -ne "\r |- spotify\n"
+echo -ne " |- zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -ne "\r |- zsh\n"
+echo -ne " |- git"
+git config --global user.name "Léo Godard"
+git config --global user.email "leo.godard@epitech.eu"
+echo -ne "\r |- git\n"
 
-case $? in
-	0) wget -q https://raw.githubusercontent.com/ungarscool1/dotfile/master/files/scripts/installGnome.sh; chmod +x installGnome.sh; ./installGnome.sh;;
-	1)	echo "Configuration de budgie décliné !";;
-	255)	echo "Configuration de budgie décliné !";;
-esac
-
-
-$DIALOG --title " Configuration " --clear --yesno "Voulez-vous configurer discord ?" 25 50
-
-case $? in
-	0) echo "Configuration de discord en cours...";	echo "Connectez-vous"; discord;	echo "Configuration de discord, terminé";;
-	1)	echo "Configuration de discord décliné !";;
-	255)	echo "Configuration de discord décliné !";;
-esac
-
-
-$DIALOG --title " Configuration " --clear --yesno "Voulez-vous configurer spotify ?" 25 50
-
-case $? in
-	0) echo "Configuration de spotify en cours...";	echo "Connectez-vous et fermez spotify une fois connecté"; spotify;	echo "Configuration de spotify, terminé";;
-	1)	echo "Configuration de spotify décliné !";;
-	255)	echo "Configuration de spotify décliné !";;
-esac
-
-$DIALOG --title " Configuration " --clear --yesno "Voulez-vous configurer Messages Android ?" 25 50
-
-case $? in
-	0) 	wget -q https://raw.githubusercontent.com/ungarscool1/dotfile/master/scripts/compileMessage.sh; chmod +x compileMessage.sh; ./compileMessage.sh;;
-	1)	echo "Configuration de Messages Android décliné !";;
-	255)	echo "Configuration de Messages Android décliné !";;
-esac
-
-
-$DIALOG --title " Configuration " --clear --yesno "Voulez-vous configurer .bashrc ?" 25 50
-
-case $? in
-	0) echo "Configuration de .bashrc en cours...";	rm ~/.bashrc;	wget -q https://raw.githubusercontent.com/ungarscool1/dotfile/master/files/.bashrc; mv .bashrc ~/;	echo "Configuration de .bashrc, terminé";;
-	1)	echo "Configuration de .bashrc décliné !";;
-	255)	echo "Configuration de .bashrc décliné !";;
-esac
-
-$DIALOG --title " Configuration " --clear --yesno "Êtes-vous en dual boot avec Windows 10 (Legacy) ?" 25 50
-
-case $? in
-	0) 	wget -q https://raw.githubusercontent.com/ungarscool1/dotfile/master/scripts/dualBoot.sh; chmod +x dualBoot.sh; ./dualBoot.sh;;
-	1)	echo "Configuration de grub décliné !";;
-	255)	echo "Configuration de grub décliné !";;
-esac
-
-wget -q https://raw.githubusercontent.com/ungarscool1/dotfile/master/scripts/patch.sh
-chmod +x patch.sh
-./patch.sh
-
-$DIALOG --title " Terminé " --clear --yesno "Voulez-vous redémarrer l'ordinateur, afin d'appliqué toutes les installations de pilotes et autres ?" 25 50
-
-case $? in
-	0) 	sudo shutdown -r now;;
-	1)	echo "";;
-	255)	echo "";;
-esac
+rm packages.list
 
 clear
